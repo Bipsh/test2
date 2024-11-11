3f0d0437e66aaefb40d47ed0d22bf80089a7f200
Aleksei <a.dr.99@mail.ru>
2024-11-11 18:36:23 +0500

    adding
    variable and function

--- theFirstFile.js
+++ theFirstFile.js
@@ -1,11 +1,16 @@
 /*alert("Hello, World!");
-console.log("Hello, world!")
+console.log("Hello, world!")*/
 
 let firstNumber = 2;
 
-let summary = 2 + 2;
+/*let summary = 2 + 2;
 console.log(summary);*/
 
 function add(a, b) {
-    console.log(a + b);
+    return a + b;
 }
+let number1 = parseFloat(prompt("Введите первое число:"));
+let number2 = parseFloat(prompt("Введите второе число:"));
+let result = add(number1, number2);
+console.log(firstNumber + ", " + result);
+console.log(firstNumber + result);
