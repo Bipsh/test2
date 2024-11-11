1a22855726d952f9ebeda90ac63794d55a553a8b
Aleksei <a.dr.99@mail.ru>
2024-11-11 19:03:30 +0500

    result
    variable and Math.pow

--- theFirstFile.js
+++ theFirstFile.js
@@ -9,8 +9,9 @@ console.log(summary);*/
 function add(a, b) {
     return a + b;
 }
-let number1 = parseFloat(prompt("Введите первое число:"));
-let number2 = parseFloat(prompt("Введите второе число:"));
+let number1 = parseFloat(prompt("Введите первое число для сложения:"));
+let number2 = parseFloat(prompt("Введите второе число для сложения:"));
+let number3 = parseFloat(prompt("Введите число, которое нужно возвести в степень:"));
 let result = add(number1, number2);
-console.log(firstNumber + ", " + result);
-console.log(firstNumber + result);
+Math.pow(number3, result);
+console.log("Переменная: " + firstNumber + ", ", "Число " + number3 + " в степени " + result + " равно " + Math.pow(number3, result));
