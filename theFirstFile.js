681f67184649033cab84e73537f27014cfc3ed65
Aleksei <a.dr.99@mail.ru>
2024-11-12 13:34:01 +0500

    last dance

--- theFirstFile.js
+++ theFirstFile.js
@@ -9,5 +9,23 @@ const number1 = array[randomIndexOne];
 const number2 = array[randomIndexTwo];
 console.log("Первое число: " + number1, "Второе число: " + number2);
 let sum = number1 + number2;
-console.log("Итоговое значение возведенное в степень цикла: " + Math.pow(sum, i));
+if (Math.pow(sum, i) % 2 === 0) {
+    console.log("Итоговое значение четное: " + Math.pow(sum, i));
+} else {
+    console.log("Итоговое значение нечетное: " + Math.pow(sum, i));
+}
+let n = Math.pow(sum, i);
+const isPowerOfTwo = n => {
+    if (n > 0 && (n & (n - 1)) === 0) {
+        let power = 0;
+        let value = 1;
+        while (value < n) {
+            value *= 2;
+            power++;
+        }
+        return `Число ${n} является степенью 2: 2^${power}`;
+    }
+    return `Число ${n} не является степенью 2.`;
+};
+console.log(isPowerOfTwo(n));
 }
